370656d06d0a9492e77f155fb5463b56cf24d359
nomvula <nzshabangu95@gmail.com>
2023-09-08 05:25:33 +0200

    Add project files.

---
+++ advisor2/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnShow = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.txtPart1 = New System.Windows.Forms.TextBox()
+        Me.txtPart2 = New System.Windows.Forms.TextBox()
+        Me.txtPart3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnShow
+        '
+        Me.btnShow.Location = New System.Drawing.Point(240, 293)
+        Me.btnShow.Name = "btnShow"
+        Me.btnShow.Size = New System.Drawing.Size(112, 39)
+        Me.btnShow.TabIndex = 0
+        Me.btnShow.Text = "show message"
+        Me.btnShow.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(479, 293)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(78, 39)
+        Me.btnCancel.TabIndex = 1
+        Me.btnCancel.Text = "cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(236, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(180, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "title bar for message==>"
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Location = New System.Drawing.Point(500, 49)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(261, 26)
+        Me.txtTitle.TabIndex = 3
+        '
+        'txtPart1
+        '
+        Me.txtPart1.Location = New System.Drawing.Point(500, 98)
+        Me.txtPart1.Name = "txtPart1"
+        Me.txtPart1.Size = New System.Drawing.Size(261, 26)
+        Me.txtPart1.TabIndex = 4
+        '
+        'txtPart2
+        '
+        Me.txtPart2.Location = New System.Drawing.Point(500, 148)
+        Me.txtPart2.Name = "txtPart2"
+        Me.txtPart2.Size = New System.Drawing.Size(261, 26)
+        Me.txtPart2.TabIndex = 5
+        '
+        'txtPart3
+        '
+        Me.txtPart3.Location = New System.Drawing.Point(500, 205)
+        Me.txtPart3.Name = "txtPart3"
+        Me.txtPart3.Size = New System.Drawing.Size(261, 26)
+        Me.txtPart3.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(251, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(165, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Part 1 of message==>"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(251, 151)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(169, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Part 2 of Message ==>"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(247, 205)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(169, 20)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Part 3 of message ==>"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtPart3)
+        Me.Controls.Add(Me.txtPart2)
+        Me.Controls.Add(Me.txtPart1)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnShow)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnShow As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtPart1 As TextBox
+    Friend WithEvents txtPart2 As TextBox
+    Friend WithEvents txtPart3 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ advisor2/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim title As String
+        Dim msg As String
+        title = txtTitle.Text
+
+        Dim Part1 As String
+        Part1 = txtPart1.Text
+        Dim Part2 As String
+        Part2 = txtPart2.Text
+        Dim Part3 As String
+        Part3 = txtPart3.Text
+        msg = MsgBox(Part1 & vbNewLine & Part2 & vbNewLine & Part3, MsgBoxStyle.DefaultButton1, title)
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Close()
+    End Sub
+End Class
